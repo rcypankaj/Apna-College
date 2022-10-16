8410dc7df90216445766aa6cf9c1d62a60acd8ca
Pankaj <rcypankaj@gmail.com>
2022-10-16 20:25:55 +0530

    Initial Commit

---
+++ src/BitManipulation/ClearBit.java
@@ -0,0 +1,22 @@
+package BitManipulation;
+
+import java.util.Scanner;
+// Clear Bit
+// Start
+public class ClearBit {
+        public static void main(String[] args) {
+            Scanner sc = new Scanner(System.in);
+            System.out.println("Enter a number");
+            int n = sc.nextInt();
+
+            System.out.println("Enter the position");
+            int pos = sc.nextInt();
+            int bitMask = 1 << pos;
+            int notBitMask = ~bitMask;
+
+            int res = n & notBitMask;
+            System.out.println(res);
+        }
+}
+
+// End
