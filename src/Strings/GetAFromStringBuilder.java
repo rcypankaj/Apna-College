b5f921c5ba1a6dd04cae58a539f45963182d10ff
Pankaj <rcypankaj@gmail.com>
2022-10-16 19:40:10 +0530

    Initial Commit

---
+++ src/Strings/GetAFromStringBuilder.java
@@ -0,0 +1,13 @@
+package Strings;
+
+import java.util.Scanner;
+// Get A Character from Index
+// Start
+public class GetAFromStringBuilder {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        StringBuilder sb = new StringBuilder("Tony");
+        System.out.println(sb.charAt(0));
+    }
+}
+// End
