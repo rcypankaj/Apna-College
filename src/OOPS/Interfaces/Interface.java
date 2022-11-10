70c7794da14267f7c56ba966771fba8b86047e4f
Pankaj <rcypankaj@gmail.com>
2022-11-10 16:56:40 +0530

    Initial Commit

---
+++ src/OOPS/Interfaces/Interface.java
@@ -0,0 +1,23 @@
+package OOPS.Interfaces;
+interface Animal {
+    void walk();
+}
+
+class Horse implements Animal {
+    public void walk() {
+        System.out.println("Horse walks on 4 legs");
+    }
+}
+
+class Chicken implements Animal {
+    public void walk() {
+        System.out.println("Chicken walks on 2 legs");
+    }
+}
+
+public class Interface {
+    public static void main(String[] args) {
+        Horse horse = new Horse();
+        horse.walk();
+    }
+}
